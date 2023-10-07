815aa9e74576276057a6f222e602bc230035afbc
Vladislav Kolesnik <97vladik@gmail.com>
2023-10-07 12:05:37 +0300

    eze

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function getElementWidth(content, padding, border) {
+
+    const contentNum = parseInt(content);
+
+    const paddingNum = parseInt(padding);
+    const paddingSize = paddingNum * 2;
+
+    const borderNum = parseFloat(border);
+    const borderSize = borderNum * 2
+    
+    console.log(contentNum + paddingSize + borderSize);
+    
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
